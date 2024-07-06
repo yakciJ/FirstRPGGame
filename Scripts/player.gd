0093287726e411a06db6ee0944196f7e2579aa98
Jickay <sieunhan482@gmail.com>
2024-07-07 00:29:05 +0700

    update player movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,23 +2,25 @@ extends CharacterBody2D
 
 
 const SPEED = 80.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var animated_sprite_2d = $AnimatedSprite2D
+var directionRightLeft = true
 
 func _physics_process(delta):
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * SPEED
+	if direction.x == 0:
+		animated_sprite_2d.play("idleRightAndLeft")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		if direction.x > 0:
+			animated_sprite_2d.play("moveLeftAndRight")
+			animated_sprite_2d.flip_h = false;
+			directionRightLeft = true
+		elif direction.x < 0:
+			animated_sprite_2d.play("moveLeftAndRight")
+			animated_sprite_2d.flip_h = true;
+			directionRightLeft = false
 	move_and_slide()
+
